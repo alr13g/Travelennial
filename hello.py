abbbdb70e8eb8b71d6541501642dd64694c855f2
Testatastic <testajoey@hotmail.com>
2017-07-19 17:14:21 -0400

    Added more code maps should be done by tonight or tomorrow

--- hello.py
+++ hello.py
@@ -2,9 +2,45 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class airport:
+	def __init__(self, name, key, lat, lng):
+		self.name = name
+		self.key = key
+		self.lat = lat
+		self.lng = lng
+
+'''
+This is just a tempate but from the video im watching he uses the key
+as a part of the web page traversal. So im thinking when we crawl airports for
+locations we see if they already exist within our database. if not we add them and make
+their custom key be the first letter of each word. if we have a key conflict then
+we take the second letter from the first word infront of the first words location.
+
+We are also should store the information so that we dont backtrack but my idea is a list of lists ie
+
+[[Airport1, Price1], [Airport2, Price2], ect]
+
+price2 is amount of money from airport 1 to airport 2
+
+We should also probably have them start and end at an airport. otherwise it might be too vauge for the result and our code.
+Like do they want to be on the north side or south side of california?
+
+Joey Testa
+'''
+airports = (
+	airport('Tallhassee International Airport', 'TIA', 30.3954, 84.3451),
+	airport('Test', 'T', 0,0)
+)
+
+airport_by_key = {airport.key: airport for airport in airports}
+
 @app.route("/")
 def home():
-	return render_template('home.html')
+	return render_template('home.html', airports = airports)
+
+@app.route('/search/<airport_code>/')
+def search():
+	return render_template('search.html', airport = airport)
 
 @app.route('/about/')
 def about():
