709eb46e394cc3ec372fbc5f65ccfea082fd5c67
Testatastic <testajoey@hotmail.com>
2017-08-02 21:07:53 -0400

    Places api now works

--- hello.py
+++ hello.py
@@ -12,25 +12,15 @@ class airport:
 		self.lat = lat
 		self.lng = lng
 
-'''
-Right now i am having issues with multiple markers. i cant have the tuple i made here properly work with the html file.
-However, if the data is on the html side it does work. REFERENCE test.html
-We are also should store the information so that we dont backtrack but my idea is a list of lists ie
-[[Airport1, Price1], [Airport2, Price2], ect]
-price2 is amount of money from airport 1 to airport 2
-We should also probably have them start and end at an airport. otherwise it might be too vauge for the result and our code.
-Like do they want to be on the north side or south side of california?
-Joey Testa
-'''
 airports = [
-	airport('Tallhassee International Airport', 'TIA', 30.395412, -84.3472458),
-	airport('Boston Logan International Airport', 'BOS', 42.3656132, -71.0117489),
-	airport('Test', 'Te', 37.9045286, -122.1445772)
+	[30.395412, -84.3472458],
+	[42.3656132, -71.0117489],
+	[37.9045286, -122.1445772]
 ]
 
 destination = airports
 
-airport_by_key = {airport.key: airport for airport in airports}
+#airport_by_key = {airport.key: airport for airport in airports}
 
 @app.route("/")
 def home():
@@ -44,11 +34,11 @@ def test():
 def search():
 	return render_template('search.html', airports = airports)
 
-@app.route('/search/<airport_code>')
-def show_route(airport_code):
-	airport = airport_by_key.get(airport_code)
+@app.route('/search/map')
+def show_route():
+	#airport = airport_by_key.get(airport_code)
 	if airport:
-		return render_template('map.html', temp = airport, destination = destination)
+		return render_template('map.html', destination = destination)
 	else:
 		return render_template('search.html', airports = airports)
 
